805d9f3fb07b12b7117de6644db986ffdbc010b9
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-01-20 22:11:08 -0300

    Fixes for build script

--- build/osx/build.sh
+++ build/osx/build.sh
@@ -6,8 +6,11 @@ NW="/Applications/node-webkit.app"
 APP='Github Pulls.app'
 
 BASE_PATH="$(perl -e "use Cwd qw(realpath);print realpath('$0');" | xargs dirname)"
+ROOT_PATH="${BASE_PATH}/../.."
 APP_PATH="$BASE_PATH/$APP"
-APP_SRC_PATH="${BASE_PATH}/../../app"
+APP_SRC_PATH="${ROOT_PATH}/app"
+BIN_PATH="${ROOT_PATH}/bin"
+BUILD_APP_PATH="$BIN_PATH/${APP}"
 
 if [[ !(-a "$NW") ]]; then
     NW="${HOME}$NW"
@@ -20,7 +23,7 @@ if [[ !(-a "$NW") ]]; then
 fi
 
 cd $APP_SRC_PATH
-npm install
+npm install --loglevel error
 cd -
 
 cp -r "$NW" "$APP_PATH"
@@ -30,6 +33,10 @@ cp -r "$APP_SRC_PATH" "$APP_PATH/Contents/Resources/app.nw"
 cp -r "${BASE_PATH}/app.icns" "$APP_PATH/Contents/Resources"
 cp -r "${BASE_PATH}/info.plist" "$APP_PATH/Contents"
 
-mv "$APP" ../../bin/
+if [[ -a "$BUILD_APP_PATH" ]]; then
+	rm -rf "$BUILD_APP_PATH"
+fi
+
+mv -f "$APP" "$BIN_PATH"
 
 echo "$APP copied to bin directory"
