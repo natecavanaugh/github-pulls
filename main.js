88c498dc7e9ec7877fb9b7d1421f76bb6761cf2d
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-09-02 08:02:14 -0700

    Extracting ghApiRequest to request module

--- main.js
+++ main.js
@@ -160,93 +160,9 @@ $(document).ready(
 			}
 		);
 
-		var defaultFailureFn = function(e) {
-			var body = $.body;
+		window.errorTemplate = errorTemplate;
 
-			var pullsTitle = $('#pullsTitle');
-
-			if (!pullsTitle.length) {
-				pullsTitle = $('#accountBar');
-			}
-
-			if (!e.statusText) {
-				e.statusText = 'An Unknown error occured when trying to load ' + url;
-			}
-
-			var errorResponse = $(errorTemplate(e));
-
-			if (pullsTitle.length) {
-				var currentError = $('.error-warning');
-
-				var showError = function() {
-					pullsTitle.after(errorResponse);
-
-					body.addClass('status-error').removeClass('status-offline').removeClass('loading');
-
-					errorResponse.addClass('alert alert-warning');
-				};
-
-				if (currentError) {
-					currentError.remove();
-
-					setTimeout(showError, 200);
-				}
-				else {
-					showError();
-				}
-			}
-			else {
-				body.addClass('status-error').removeClass('status-offline').removeClass('loading').prepend(errorResponse);
-			}
-		};
-
-		var ghApiRequest = function(path, callback, failure, config) {
-			var url = sub(API_URL, path);
-
-			var headers = {
-				'Authorization': 'token ' + settings.val('token'),
-				'User-Agent': 'Github Pulls app v1.0'
-			};
-
-			var data = null;
-
-			var method = 'GET';
-
-			if (config) {
-				headers = _.extend(headers, config.headers);
-				data = config.data || data;
-				method = config.method || method;
-			}
-
-			var x = $.ajax(
-				url,
-				{
-					headers: headers,
-					data: data,
-					dataType: 'json',
-					type: method,
-					error: function(xhr) {
-						var json = {};
-
-						if (xhr && xhr.responseText) {
-							try {
-								json = JSON.parse(xhr.responseText);
-							}
-							catch (e) {
-								// console.log(e);
-							}
-						}
-						(failure || defaultFailureFn)(json, xhr);
-					},
-					crossDomain: true,
-					success: function(json, msg) {
-						if (_.isFunction(callback)) {
-							callback(json, msg);
-						}
-					}
-				}
-			);
-		};
+		var ghApiRequest = require('./util/request')(window);
 
 		var cachedResults = '';
 
