bfe5aec3463b6621ca96c48fd107e4b4b37a0733
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-09-05 08:10:16 -0700

    Extracting login code to it's own module

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var sub = require('string-sub');
 var success = require('success');
 
 var GithubPulls = require('./lib/');
+var login = require('./lib/login');
 var pulls = require('./lib/pulls');
 
 var settings = require('./util/settings');
@@ -199,120 +200,6 @@ $(document).ready(
 				settings.destroy();
 			}
 		);
-
-		var login = function(username, password) {
-			var scriptNote = 'Github Pulls (by Liferay)';
-
-			var data = JSON.stringify(
-				{
-					scopes: ['repo'],
-					note: scriptNote
-				}
-			);
-
-			var loginData = {
-				data: data,
-				headers: {
-					'Authorization': 'Basic ' + btoa(username + ':' + password),
-					'Content-Type': 'application/x-www-form-urlencoded'
-				},
-				method: 'GET'
-			};
-
-			github.authenticate(
-				{
-					type: 'basic',
-					username: username,
-					password: password
-				}
-			);
-
-			var createToken = function() {
-				// Second passs to create it
-				github.authorization.create(
-					{
-						scopes: ['repo'],
-						note: scriptNote
-					},
-					success(handleAuthError, handleNewTokenResponse)
-				);
-			};
-
-			var checkExistingToken = function(response) {
-				// First pass to see if we have it
-				var token = _.find(
-					response,
-					{
-						note: scriptNote
-					}
-				);
-
-				if (token && token.hashed_token) {
-					github.authorization.delete(
-						{
-							id: token.id
-						},
-						success(handleAuthError, createToken)
-					);
-				}
-				else {
-					createToken();
-				}
-			};
-
-			var handleAuthError = function(err) {
-				console.log(err);
-				var response = {
-					errorText: 'Could not log into Github.',
-					statusText: 'Error',
-					message: JSON.parse(err.message).message
-				};
-
-				GithubPulls.emit('login:error', response, err);
-			};
-
-			var handleNewTokenResponse = function(response) {
-				var token = response.token;
-
-				if (token) {
-					setToken(token);
-				}
-				else {
-					handleAuthError(response);
-				}
-			};
-
-			var setToken = function(token) {
-				var val = {
-					token: token,
-					username: username
-				};
-
-				settings.val(val);
-
-				GithubPulls.emit('login:success');
-
-				github.user.get(
-					{},
-					function(err, response) {
-						if (response && response.avatar_url) {
-							avatar = response.avatar_url;
-
-							settings.val('avatar', avatar);
-						}
-
-						GithubPulls.emit('login:complete');
-					}
-				);
-			};
-
-			github.authorization.getAll(
-				{
-					per_page: 100
-				},
-				success(handleAuthError, checkExistingToken)
-			);
-		};
 
 		var loadLogin = function() {
 			var body = $.body;
