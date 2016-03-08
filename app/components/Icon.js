54c114185b071b420adf05e224d51c4733067cf2
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2016-03-08 15:21:51 -0800

    Add className prop to icon

--- app/components/Icon.js
+++ app/components/Icon.js
@@ -1,10 +1,11 @@
 import React, {PropTypes} from 'react';
 
-const Icon = ({name}) => {
-	return <svg className={`lexicon-icon lexicon-icon-${name}`}><use xlinkHref={`../bower_components/lexicon/release/images/icons/icons.svg#${name}`}></use></svg>;
+const Icon = ({className, name}) => {
+	return <svg className={`lexicon-icon lexicon-icon-${name} ${className}`}><use xlinkHref={`../bower_components/lexicon/release/images/icons/icons.svg#${name}`}></use></svg>;
 };
 
 Icon.propTypes = {
+	className: PropTypes.string,
 	name: PropTypes.string.isRequired
 };
 
