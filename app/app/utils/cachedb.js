7220937380cf0faf9c8204f2c6ab85a0c2166064
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2016-06-22 12:50:45 -0700

    Adding the batch method to the cachedb impl, will need this after github-cache is published to 1.3.0

--- app/app/utils/cachedb.js
+++ app/app/utils/cachedb.js
@@ -1,8 +1,55 @@
+import _ from 'lodash';
 import jsop from './jsop';
 
 export default class {
 	constructor(path) {
 		this.db = jsop(path);
+	}
+
+	batch(ops, options, cb) {
+		if (!_.isArray(ops)) {
+			throw new Error('The ops parameter must be an array');
+		}
+
+		if (_.isUndefined(cb)) {
+			cb = options;
+		}
+
+		if (!_.isFunction(cb)) {
+			throw new Error('The callback parameter must be a function');
+		}
+
+		var arr = _.map(
+			ops,
+			item => {
+				if (!item.type && !_.isUndefined(item.key) && !_.isUndefined(item.value)) {
+					item.type = 'put';
+				}
+
+				return item;
+			}
+		);
+
+		var errors = [];
+
+		var collectResults = function(err, res) {
+			if (err) {
+				errors.push(err);
+			}
+		};
+
+		_.forEach(
+			arr,
+			({type, key, value}) => {
+				if (type === 'del') {
+					value = collectResults;
+				}
+
+				this[type](key, value, collectResults);
+			}
+		);
+
+		cb(errors.length ? errors : null);
 	}
 
 	del(key, cb) {
